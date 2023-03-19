2b81f3ce47411a1dfb44e5412a7571262ffb3964
Wilson McGill <wrmcgill@memphis.edu>
2023-03-19 14:35:07 -0500

    navbar fix and test page

--- RubyonRails/config/routes.rb
+++ RubyonRails/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  root 'pages#home'
   get 'home', to: 'pages#home', as: 'home'
+  get 'test', to: 'pages#test', as: 'test'
   get "/users/new", to: "users#new"
   get 'users', to: 'users#index'
   get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
