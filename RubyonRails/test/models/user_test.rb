dd7f73be97ff89976d7a63fa06792e83aec29776
Hunter <hjckson6@memphis.edu>
2023-08-29 12:19:51 -0500

    Added httparty gem for API connection for license

--- RubyonRails/test/models/user_test.rb
+++ RubyonRails/test/models/user_test.rb
@@ -11,6 +11,7 @@
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  role                   :integer
+#  verification_key       :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  tenant_id              :bigint
