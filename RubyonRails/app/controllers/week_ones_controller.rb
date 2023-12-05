7b61ee4886bd066ea60a84a4efe1d1c29d915f9a
BestCatNA <lewoods101@gmail.com>
2023-12-05 17:33:28 -0500

    Dunno why this didn't work earlier, should have the proper ear toggles working in code.

--- RubyonRails/app/controllers/week_ones_controller.rb
+++ RubyonRails/app/controllers/week_ones_controller.rb
@@ -85,9 +85,9 @@ class WeekOnesController < ApplicationController
   
     
       private
-    
+    #ALLEARS - L - added left and right ear decibel
       def week_one_params
-        params.require(:week_one).permit( :notes, :client_name, :test_type, :left_score, :right_score, :ear_advantage, :ear_advantage_score)
+        params.require(:week_one).permit( :notes, :client_name, :test_type, :left_score, :right_score, :ear_advantage, :ear_advantage_score, :left_ear_decibel, :right_ear_decibel)
       end
 
 end
